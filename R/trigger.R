64255d2362879cd4ea8590ce13753d23dd7f91d8
Diego H <diego.hernangomezherrero@gmail.com>
2022-02-28 15:48:41 +0100

    Try again

--- R/trigger.R
+++ R/trigger.R
@@ -1,8 +1,13 @@
 Sys.setenv(MAPSPAIN_CACHE_DIR = "data")
 
-for (i in 1:3) {
-  source("R/01_create_map.R")
-  Sys.sleep(3)
-}
+
+source("R/01_create_map.R")
+
+message("Start with second")
+source("R/01_create_map.R")
+
+message("Start with third")
+source("R/01_create_map.R")
+
 
 rm(list = ls())
