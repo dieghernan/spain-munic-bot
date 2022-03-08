36cee05bb6027bc258d84b7d778b582d6770da7c
Diego H <diego.hernangomezherrero@gmail.com>
2022-03-08 15:00:23 +0100

    Skip errors

--- R/trigger.R
+++ R/trigger.R
@@ -1,13 +1,13 @@
 Sys.setenv(MAPSPAIN_CACHE_DIR = "data")
 
 
-source("R/01_create_map.R")
+try(source("R/01_create_map.R"))
 
 message("Start with second")
-source("R/01_create_map.R")
+try(source("R/01_create_map.R"))
 
 message("Start with third")
-source("R/01_create_map.R")
+try(source("R/01_create_map.R"))
 
 
 rm(list = ls())
